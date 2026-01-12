5eea15d8a3dfd1bee879fc1972ac51c5249a2404
juanignacioalonso <zalbak87@gmail.com>
2026-01-12 15:02:31 -0300

    inicio de  aplicacion

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,32 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+  {
+    path: 'basic',
+    title: 'Pipes Basicos',
+    loadComponent: () => import('./pages/basic-page/basic-page')
+  },
+  {
+    path: 'numbers',
+    title: 'Numbers Pipes',
+    loadComponent: () => import('./pages/numbers-page/numbers-page')
+  },
+    {
+    path: 'uncommon',
+    title: 'Pipes no tan comunes',
+    loadComponent: () => import('./pages/uncommon-page/uncommon-page')
+  },
+    {
+    path: 'custom',
+    title: 'Pipes Personalizados',
+    loadComponent: () => import('./pages/custom-page/custom-page')
+  },
+  {
+    path: '**',
+    redirectTo: 'basic'
+  }
+
+
+
+];

e305070cff85ad5c768aebd15104e065b4d4d038
juanignacioalonso <zalbak87@gmail.com>
2026-01-16 15:46:45 -0300

    Cambiar el idioma de la aplicacion

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,29 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
+import { ApplicationConfig, LOCALE_ID, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { registerLocaleData } from '@angular/common';
+
+import localEs from '@angular/common/locales/es-AR';
+import localFr from '@angular/common/locales/fr';
+import { LocaleService } from './services/locale.service';
+
+
+registerLocaleData(localEs, 'es');
+registerLocaleData(localFr, 'fr');
+
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
-    provideRouter(routes)
+    provideRouter(routes),
+
+    {
+      provide: LOCALE_ID,
+      //useValue: 'es',
+      deps:[LocaleService],
+      useFactory: (localeService:LocaleService) => localeService.getLocale,
+    }
   ]
 };
--- src/app/pages/basic-page/basic-page.ts
+++ src/app/pages/basic-page/basic-page.ts
@@ -1,18 +1,41 @@
-import { LowerCasePipe, TitleCasePipe, UpperCasePipe } from '@angular/common';
-import { Component, signal } from '@angular/core';
+import { AvailableLocale, LocaleService } from './../../services/locale.service';
+import { DatePipe, LowerCasePipe, TitleCasePipe, UpperCasePipe } from '@angular/common';
+import { Component, effect, inject, LOCALE_ID, signal } from '@angular/core';
 
 @Component({
   selector: 'app-basic-page',
   imports: [
     LowerCasePipe,
     UpperCasePipe,
-    TitleCasePipe
+    TitleCasePipe,
+    DatePipe,
   ],
   templateUrl: './basic-page.html',
 })
 export default class BasicPage {
 
+  LocaleService = inject(LocaleService);
+  currentlocale = signal(inject(LOCALE_ID));
+
   nameLower = signal('juan')
   nameUpper = signal('JUAN')
   fullName = signal('JuAn iGnAcIo')
+
+
+  customDate = signal(new Date())
+
+  tickingDateEffect = effect((onCleanup) => {
+    const interval = setInterval(() => {
+      this.customDate.set(new Date())
+    },1000);
+
+    onCleanup(() => {
+      clearInterval(interval);
+    });
+  })
+
+  changueLocale(locale:AvailableLocale){
+    console.log('Changing locale to:', locale);
+    this.LocaleService.changeLocale(locale);
+  }
 }
